08e5d801970fbeb18c382be6290c44227780d8cb
Thomas Seidl <remus@gmx.net>
2010-11-29 13:08:30 +0000

    #984134: Add Solr-specific query alter hooks.

---
+++ search_api_solr.api.php
@@ -0,0 +1,57 @@
+<?php
+// $Id$
+
+/**
+ * @file
+ * Hooks provided by the Search API Solr search module.
+ */
+
+/**
+ * @addtogroup hooks
+ * @{
+ */
+
+/**
+ * Lets modules alter a Solr search request before sending it.
+ *
+ * Apache_Solr_Service::search() is called afterwards with these parameters.
+ * Please see this method for details on what should be altered where and what
+ * is set afterwards.
+ *
+ * @param array $call_args
+ *   An associative array containing all four arguments to the
+ *   Apache_Solr_Service::search() call ("query", "offset", "limit" and
+ *   "params") as references.
+ * @param SearchApiQueryInterface $query
+ *   The SearchApiQueryInterface object representing the executed search query.
+ */
+function hook_search_api_solr_query_alter(array &$call_args, SearchApiQueryInterface $query) {
+  if ($query->getOption('foobar')) {
+    $call_args['params']['foo'] = 'bar';
+  }
+}
+
+/**
+ * Lets modules alter a Solr search request for a multi-index search before
+ * sending it.
+ *
+ * Apache_Solr_Service::search() is called afterwards with these parameters.
+ * Please see this method for details on what should be altered where and what
+ * is set afterwards.
+ *
+ * @param array $call_args
+ *   An associative array containing all four arguments to the
+ *   Apache_Solr_Service::search() call ("query", "offset", "limit" and
+ *   "params") as references.
+ * @param SearchApiMultiQueryInterface $query
+ *   The object representing the executed search query.
+ */
+function hook_search_api_solr_query_alter(array &$call_args, SearchApiMultiQueryInterface $query) {
+  if ($query->getOption('foobar')) {
+    $call_args['params']['foo'] = 'bar';
+  }
+}
+
+/**
+ * @} End of "addtogroup hooks".
+ */
