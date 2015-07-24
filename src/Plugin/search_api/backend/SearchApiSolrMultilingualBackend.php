99c2bf1000c5754dbad77fd350380b4286ea51eb
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-07-24 18:01:37 +0200

    first draft of preQuery()

--- src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
@@ -7,9 +7,11 @@
 
 namespace Drupal\apachesolr_multilingual\Plugin\search_api\backend;
 
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\search_api_solr\Plugin\search_api\backend\SearchApiSolrBackend;
 use Drupal\search_api\Query\QueryInterface;
 use Drupal\search_api\Query\ResultSetInterface;
+use Solarium\QueryType\Select\Query\FilterQuery;
 use Solarium\QueryType\Select\Query\Query;
 
 /**
@@ -31,7 +33,29 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
    *   search query.
    */
   protected function preQuery(Query $solarium_query, QueryInterface $query) {
-    $this->addNativeQueryParams($solarium_query, $query);
+    parent::preQuery($solarium_query, $query);
+
+    // @todo $language_id needs to be set dynamically from filter, config,
+    //   facet, whatever ...
+    $language_id = \Drupal::languageManager()->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
+
+    $edismax = $solarium_query->getEDisMax();
+    $query_fields = $edismax->getQueryFields();
+
+    $index = $query->getIndex();
+    $fulltext_fields = $index->getFulltextFields(TRUE);
+    $field_names = $this->getFieldNames($index);
+
+    foreach($fulltext_fields as $fulltext_field) {
+      $query_fields = str_replace($field_names[$fulltext_field], 'i18n_' . $language_id . '_' . $field_names[$fulltext_field], $query_fields);
+    }
+
+    $edismax->setQueryFields($query_fields);
+
+    $fq = new FilterQuery();
+    $fq->setKey('i18n_language');
+    $fq->setQuery($field_names['search_api_language'] . ':' . $language_id);
+    $solarium_query->addFilterQuery($fq);
   }
 
   /**
@@ -46,26 +70,7 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
    *   The response object returned by Solr.
    */
   protected function postQuery(ResultSetInterface $results, QueryInterface $query, $response) {
+    parent::postQuery($results, $query, $response);
   }
 
-
-  /**
-   * Add native solr query params to the query.
-   *
-   * The native solr queryp params are set via QueryInterface::setOption() and
-   * are prefixed by 'solr_'.
-   *
-   * @param \Solarium\QueryType\Select\Query\Query $solarium_query
-   *   The Solarium select query object.
-   * @param \Drupal\search_api\Query\QueryInterface $query
-   *   The \Drupal\search_api\Query\Query object representing the executed
-   *   search query.
-   */
-  protected function addNativeQueryParams(Query $solarium_query, QueryInterface $query) {
-    foreach ($query->getOptions() as $option => $value) {
-      if (strpos($option, 'solr_') === 0) {
-        $solarium_query->addParam(substr($option, 5), $value);
-      }
-    }
-  }
 }
