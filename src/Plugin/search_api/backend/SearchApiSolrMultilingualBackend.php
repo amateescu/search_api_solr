b92c757a4733905af5c8b30e88767c290ce97a01
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-07-29 16:45:25 +0200

    first draft of extractResults()

--- src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
@@ -12,8 +12,12 @@ use Drupal\search_api\IndexInterface;
 use Drupal\search_api\Query\QueryInterface;
 use Drupal\search_api\Query\ResultSetInterface;
 use Drupal\search_api_solr\Plugin\search_api\backend\SearchApiSolrBackend;
+use Solarium\Core\Client\Response;
 use Solarium\QueryType\Select\Query\FilterQuery;
 use Solarium\QueryType\Select\Query\Query;
+use Solarium\QueryType\Select\Result\Result;
+
+define('SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR', '_' . bin2hex('|') . '_');
 
 /**
  * @SearchApiBackend(
@@ -48,15 +52,42 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
     $field_names = $this->getFieldNames($index);
 
     foreach($fulltext_fields as $fulltext_field) {
-      $query_fields = str_replace($field_names[$fulltext_field], 'i18n_' . $language_id . '_' . $field_names[$fulltext_field], $query_fields);
+      $query_fields = str_replace(
+        $field_names[$fulltext_field],
+        $this->getMultilingualSolrFieldName($field_names[$fulltext_field], $language_id),
+        $query_fields
+      );
     }
 
     $edismax->setQueryFields($query_fields);
 
     $fq = new FilterQuery();
-    $fq->setKey('i18n_language');
+    $fq->setKey('i18n_' . $language_id);
     $fq->setQuery($field_names['search_api_language'] . ':' . $language_id);
     $solarium_query->addFilterQuery($fq);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  protected function extractResults(QueryInterface $query, Result $result) {
+    if ($this->configuration['retrieve_data']) {
+      $data = $result->getData();
+      foreach ($data['docs'] as &$doc) {
+        foreach (array_keys($doc) as $field_name) {
+          $new_field_name = preg_replace('/^([a-z]+)' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '.*?' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '/', '$1', $field_name);
+          if ($new_field_name != $field_name) {
+            $doc[$new_field_name] = $doc[$field_name];
+            unset($doc[$field_name]);
+          }
+        }
+      }
+
+      $new_response = new Response(json_encode($data), $result->getResponse()->getHeaders());
+      $result = new Result(NULL, $result->getQuery(), $new_response);
+    }
+
+    return parent::extractResults($query, $result);
   }
 
   /**
@@ -72,7 +103,7 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
    */
   protected function postQuery(ResultSetInterface $results, QueryInterface $query, $response) {
     parent::postQuery($results, $query, $response);
-  }
+ }
 
    /**
    * Applies custom modifications to indexed Solr documents.
@@ -111,7 +142,23 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
     }
   }
 
-  private function getMultilingualSolrFieldName($field_name, $language_id) {
-    return preg_replace('/^([a-z]+)/', '$1_i18n_' . $language_id, $field_name);
+  /**
+   * Maps a solr field name to its language specific equivalent.
+   *
+   * For example the dynamic field tm_* will become tm_|_i18n_en_|_* for English.
+   * Following this pattern we also have fallbacks automatically:
+   * - tm_|_i18n_de_AT_|_*
+   * - tm_|_i18n_de_|_*
+   * - tm_*
+   * This concept bases on the fact that "longer patterns will be matched first.
+   * If equal size patterns both match,the first appearing in the schema will be
+   * used." See https://wiki.apache.org/solr/SchemaXml#Dynamic_fields
+   *
+   * @param type $field_name
+   * @param type $language_id
+   * @return type
+   */
+  protected function getMultilingualSolrFieldName($field_name, $language_id) {
+    return preg_replace('/^([a-z]+)/', '$1' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . 'i18n_' . $language_id . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR, $field_name);
   }
 }
