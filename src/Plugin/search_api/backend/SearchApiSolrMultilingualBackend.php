06d9202b8de057630a379f9417790a3e0597f7d2
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2015-07-30 14:26:03 +0200

    introduced naming and fallback schema for language specific fields according to https://github.com/amateescu/search_api_solr/pull/67

--- src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrMultilingualBackend.php
@@ -8,6 +8,7 @@
 namespace Drupal\apachesolr_multilingual\Plugin\search_api\backend;
 
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\apachesolr_multilingual\Utility\Utility as SearchApiSolrMultilingualUtility;
 use Drupal\search_api\IndexInterface;
 use Drupal\search_api\Query\QueryInterface;
 use Drupal\search_api\Query\ResultSetInterface;
@@ -16,8 +17,6 @@ use Solarium\Core\Client\Response;
 use Solarium\QueryType\Select\Query\FilterQuery;
 use Solarium\QueryType\Select\Query\Query;
 use Solarium\QueryType\Select\Result\Result;
-
-define('SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR', '_' . bin2hex('|') . '_');
 
 /**
  * @SearchApiBackend(
@@ -54,7 +53,7 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
     foreach($fulltext_fields as $fulltext_field) {
       $query_fields = str_replace(
         $field_names[$fulltext_field],
-        $this->getMultilingualSolrFieldName($field_names[$fulltext_field], $language_id),
+        SearchApiSolrMultilingualUtility::getLanguageSpecificSolrDynamicFieldNameForSolrDynamicFieldName($field_names[$fulltext_field], $language_id),
         $query_fields
       );
     }
@@ -74,11 +73,11 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
     if ($this->configuration['retrieve_data']) {
       $data = $result->getData();
       foreach ($data['docs'] as &$doc) {
-        foreach (array_keys($doc) as $field_name) {
-          $new_field_name = preg_replace('/^([a-z]+)' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '.*?' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '/', '$1', $field_name);
-          if ($new_field_name != $field_name) {
-            $doc[$new_field_name] = $doc[$field_name];
-            unset($doc[$field_name]);
+        foreach (array_keys($doc) as $language_specific_field_name) {
+          $field_name = SearchApiSolrMultilingualUtility::getSolrDynamicFieldNameForLanguageSpecificSolrDynamicFieldName($language_specific_field_name);
+          if ($field_name != $language_specific_field_name) {
+            $doc[$field_name] = $doc[$language_specific_field_name];
+            unset($doc[$language_specific_field_name]);
           }
         }
       }
@@ -136,29 +135,10 @@ class SearchApiSolrMultilingualBackend extends SearchApiSolrBackend {
       $language_id = $fields[$field_names['search_api_language']];
       foreach ($fields as $field_name => $field_value) {
         if (in_array($field_name, $fulltext_field_names)) {
-          $document->addField($this->getMultilingualSolrFieldName($field_name, $language_id), $field_value, $document->getFieldBoost($field_name));
+          $document->addField(SearchApiSolrMultilingualUtility::getLanguageSpecificSolrDynamicFieldNameForSolrDynamicFieldName($field_name, $language_id), $field_value, $document->getFieldBoost($field_name));
         }
       }
     }
   }
 
-  /**
-   * Maps a solr field name to its language specific equivalent.
-   *
-   * For example the dynamic field tm_* will become tm_|_i18n_en_|_* for English.
-   * Following this pattern we also have fallbacks automatically:
-   * - tm_|_i18n_de_AT_|_*
-   * - tm_|_i18n_de_|_*
-   * - tm_*
-   * This concept bases on the fact that "longer patterns will be matched first.
-   * If equal size patterns both match,the first appearing in the schema will be
-   * used." See https://wiki.apache.org/solr/SchemaXml#Dynamic_fields
-   *
-   * @param type $field_name
-   * @param type $language_id
-   * @return type
-   */
-  protected function getMultilingualSolrFieldName($field_name, $language_id) {
-    return preg_replace('/^([a-z]+_)/', '$1' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . 'i18n_' . $language_id . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR, $field_name);
-  }
 }
---
+++ src/Utility/Utility.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\apachesolr_multilingual\Utility.
+ */
+
+namespace Drupal\apachesolr_multilingual\Utility;
+
+use Drupal\search_api_solr\Utility\Utility as SearchApiSolrUtility;
+
+/**
+ * The separator to encapsultate the language id. We must not use any character
+ * that has a special meaning within a regular expression. See
+ * http://de2.php.net/manual/en/regexp.reference.meta.php
+ * Additionally we have to avoid ':', '_' and '/' which are already reserved by
+ * search_api. '@' must not be used as well because it's common for locales like
+ * "de_DE@euro".
+ */
+define('SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR', ';');
+
+class Utility {
+
+  /**
+   * Maps a solr field name to its language specific equivalent.
+   *
+   * For example the dynamic field tm* will become tm;en;* for English.
+   * Following this pattern we also have fallbacks automatically:
+   * - tm;de_AT;*
+   * - tm;de;*
+   * - tm*
+   * This concept bases on the fact that "longer patterns will be matched first.
+   * If equal size patterns both match,the first appearing in the schema will be
+   * used." See https://wiki.apache.org/solr/SchemaXml#Dynamic_fields
+   * Note that the real field name for solr is encoded. So the real values for
+   * the example above are:
+   * - tm_3b_de_5f_AT_3b_*
+   * - tm_3b_de_3b_*
+   * - tm_*
+   * See Drupal\search_api_solr\Utility\Utility::encodeSolrDynamicFieldName().
+   *
+   * @param type $field_name
+   * @param type $language_id
+   * @return type
+   */
+  public static function getLanguageSpecificSolrDynamicFieldNameForSolrDynamicFieldName($field_name, $language_id) {
+    return Utility::_modifySolrDynamicFieldName($field_name, '/^([a-z]+)_/', '$1' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . $language_id . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR);
+  }
+
+  public static function getSolrDynamicFieldNameForLanguageSpecificSolrDynamicFieldName($field_name) {
+    return Utility::_modifySolrDynamicFieldName($field_name, '/' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '.+?' . SEARCH_API_SOLR_MULTILINGUAL_LANGUAGE_SEPARATOR . '/');
+  }
+
+  public static function _modifySolrDynamicFieldName($field_name, $pattern, $replacement = '') {
+    $encoded = strpos($field_name, SearchApiSolrUtility::encodeSolrDynamicFieldName('_'));
+    if ($encoded) {
+      $field_name = SearchApiSolrUtility::decodeSolrDynamicFieldName($field_name);
+    }
+    $field_name = preg_replace($pattern, $replacement, $field_name);
+    if ($encoded) {
+      $field_name = SearchApiSolrUtility::encodeSolrDynamicFieldName($field_name);
+    }
+    return $field_name;
+  }
+}
+
+
