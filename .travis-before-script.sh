12429d5d09f562569d2d46c37a52ccaa130882f7
Lukas Schneider <lukas.schneider.1990@outlook.com>
2015-08-05 16:47:50 +0200

    readded notification info, added SearchAPI to before-install download list, tried readding correct lines

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -13,5 +13,9 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 mkdir -p "$DRUPAL_TI_MODULES_PATH"
 cd "$DRUPAL_TI_MODULES_PATH"
 
-# Download solr
+# Download Search API and solr
+git clone --branch 8.x-1.x http://git.drupal.org/project/search_api.git modules/search_api
 curl -sSL https://raw.githubusercontent.com/nickveenhof/travis-solr/master/travis-solr.sh | bash
+
+# Make sure the apache root is in our wanted directory
+echo "$(curl -fsSL https://gist.githubusercontent.com/nickveenhof/11386315/raw/b8abaf9304fe12b5cc7752d39c29c1edae8ac2e6/gistfile1.txt)" | sed -e "s,PATH,$TRAVIS_BUILD_DIR/../drupal,g" | sudo tee /etc/apache2/sites-available/default > /dev/null
