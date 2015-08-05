d8779fa2054a4d2325c0314d0f7e4c510c9632d2
Lukas Schneider <lukas.schneider.1990@outlook.com>
2015-08-05 16:16:54 +0200

    switched to Drupal_TI

---
+++ .travis-before-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Add needed dependencies.
+cd "$DRUPAL_TI_DRUPAL_DIR"
+
+# These variables come from environments/drupal-*.sh
+mkdir -p "$DRUPAL_TI_MODULES_PATH"
+cd "$DRUPAL_TI_MODULES_PATH"
+
+# Download solr
+curl -sSL https://raw.githubusercontent.com/nickveenhof/travis-solr/master/travis-solr.sh | bash
