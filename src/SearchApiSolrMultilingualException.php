ea221e18536fa325b649210387c504934bc14a9c
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2016-03-18 14:48:40 +0100

    derive from SearchApiSolrException

--- src/SearchApiSolrMultilingualException.php
+++ src/SearchApiSolrMultilingualException.php
@@ -7,9 +7,9 @@
 
 namespace Drupal\search_api_solr_multilingual;
 
-use Drupal\search_api\SearchApiException;
+use Drupal\search_api_solr\SearchApiSolrException;
 
 /**
- * Represents an exception that occurres in Search API Solr Multilingual.
+ * Represents an exception that occurs in Search API Solr Multilingual.
  */
-class SearchApiSolrMultilingualException extends SearchApiException { }
+class SearchApiSolrMultilingualException extends SearchApiSolrException { }
