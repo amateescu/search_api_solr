f8066b6b68f0cd5d47b1c773204ecbcbb6928893
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2016-03-22 15:02:25 +0100

    fixed race condition between hoock_update_N and post update depending on how many updates the user skipped.

--- search_api_solr_multilingual.post_update.php
+++ search_api_solr_multilingual.post_update.php
@@ -4,16 +4,8 @@
  * Re-installs all Solr Field Types to be compatible to the latest structure.
  */
 function search_api_solr_multilingual_post_update_replace_solr_field_types() {
-  $storage = \Drupal::entityTypeManager()->getStorage('solr_field_type');
-  $storage->delete($storage->loadMultiple());
-
-  /** @var \Drupal\Core\Config\ConfigInstallerInterface $config_installer */
-  $config_installer = \Drupal::service('config.installer');
-  $config_installer->installDefaultConfig('module', 'search_api_solr_multilingual');
-  $restrict_by_dependency = [
-    'module' => 'search_api_solr_multilingual',
-  ];
-  $config_installer->installOptionalConfig(NULL, $restrict_by_dependency);
+  // Removed due to race condition with
+  // search_api_solr_multilingual_update_8001().
 }
 
 /**
@@ -27,4 +19,11 @@ function search_api_solr_multilingual_post_update_fix_backend_ids() {
     $server->set('backend', 'search_api_solr_multilingual');
     $server->save();
   }
+}
+
+/**
+ * Re-installs all Solr Field Types to be compatible to the latest structure.
+ */
+function search_api_solr_multilingual_post_update_replace_solr_field_types_2() {
+  search_api_solr_multilingual_delete_and_reinstall_all_field_types();
 }
