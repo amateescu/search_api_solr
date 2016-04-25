d54b149146d54572b674f7b1795e5541291e5d87
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2016-04-25 19:03:14 +0200

    added first tests

---
+++ Tests/tests/src/Kernel/SearchApiSolrMultilingualTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\search_api_solr_multilingual\Tests\SearchApiSolrMultilingualTest.
+ */
+
+namespace Drupal\Tests\search_api_solr_multilingual\Kernel;
+
+use Drupal\Tests\search_api_solr\Kernel\SearchApiSolrTest;
+
+/**
+ * Tests index and search capabilities using the Solr search backend.
+ *
+ * @group search_api_solr
+ */
+class SearchApiSolrMultilingualTest extends SearchApiSolrTest {
+
+  /**
+   * Modules to enable for this test.
+   *
+   * @var string[]
+   */
+  public static $modules = array(
+    'search_api_solr_multilingual',
+    'search_api_test_solr_multilingual',
+  );
+
+  /**
+   * A Search API server ID.
+   *
+   * @var string
+   */
+  protected $serverId = 'solr_multilingual_search_server';
+
+  /**
+   * A Search API index ID.
+   *
+   * @var string
+   */
+  protected $indexId = 'solr_multilingual_search_index';
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->installConfig(array('search_api_test_solr_multilingual'));
+    parent::setUp();
+  }
+
+}
---
+++ src/Tests/IntegrationTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\search_api_solr\Tests\IntegrationTest.
+ */
+
+namespace Drupal\search_api_solr_multilingual\Tests;
+
+/**
+ * Tests the overall functionality of the Search API framework and admin UI.
+ *
+ * @group search_api_solr_multilingual
+ */
+class IntegrationTest extends \Drupal\search_api_solr\Tests\IntegrationTest {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $serverBackend = 'search_api_solr_multilingual';
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = array(
+    'search_api_solr_multilingual',
+  );
+
+}
