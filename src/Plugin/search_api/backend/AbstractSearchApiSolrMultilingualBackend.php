d2ced3bb08da155a83161880b9bf42cf694bc3bb
Christian Spitzlay <christian.spitzlay@bio.logis.de>
2016-06-02 15:10:47 +0200

    Fix error message text: field dynamic -> dynamic field

--- src/Plugin/search_api/backend/AbstractSearchApiSolrMultilingualBackend.php
+++ src/Plugin/search_api/backend/AbstractSearchApiSolrMultilingualBackend.php
@@ -210,11 +210,11 @@ abstract class AbstractSearchApiSolrMultilingualBackend extends SearchApiSolrBac
                 '%field' => $language_specific_field,
               );
               if ($this->hasLanguageUndefinedFallback()) {
-                \Drupal::logger('search_api_solr_multilingual')->warning('Error while searching: language specific field dynamic %field is not defined in the schema.xml, fallback to language unspecific field is enabled.', $vars);
+                \Drupal::logger('search_api_solr_multilingual')->warning('Error while searching: language-specific dynamic field %field is not defined in the schema.xml, fallback to language-unspecific field is enabled.', $vars);
                 $language_specific_fields[] = $language_specific_field . $boost;
               }
               else {
-                \Drupal::logger('search_api_solr_multilingual')->error('Error while searching: language specific field dynamic %field is not defined in the schema.xml, fallback to language unspecific field is not enabled.', $vars);
+                \Drupal::logger('search_api_solr_multilingual')->error('Error while searching: language-specific dynamic field %field is not defined in the schema.xml, fallback to language-unspecific field is not enabled.', $vars);
               }
             }
           }
@@ -404,7 +404,7 @@ abstract class AbstractSearchApiSolrMultilingualBackend extends SearchApiSolrBac
               $vars = array(
                 '%field' => $multilingual_solr_field_name,
               );
-              \Drupal::logger('search_api_solr_multilingual')->warning('Error while indexing: language specific field dynamic %field is not defined in the schema.xml, fallback to language unspecific field is enabled.', $vars);
+              \Drupal::logger('search_api_solr_multilingual')->warning('Error while indexing: language-specific dynamic field %field is not defined in the schema.xml, fallback to language-unspecific field is enabled.', $vars);
               unset($field_name_map_per_language[$language_id][$monolingual_solr_field_name]);
             }
             else {
