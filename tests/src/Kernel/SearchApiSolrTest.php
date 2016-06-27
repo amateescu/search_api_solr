89796d22fc93b471e8acc23d7f0dd9375133a730
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2016-06-27 13:09:52 +0200

    fixed test tagging of a single filter query of a facet query

--- tests/src/Kernel/SearchApiSolrTest.php
+++ tests/src/Kernel/SearchApiSolrTest.php
@@ -402,8 +402,10 @@ class SearchApiSolrTest extends BackendTestBase {
     );
     $query->setOption('search_api_facets', $facets);
     $fq = $this->invokeMethod($backend, 'getFilterQueries', [$query, $mapping, $fields]);
-    $this->assertEquals('{!tag=facet:tagtosearchfor}ss_category:"article_category"', $fq[0], 'Tag found in tagged first filter query');
-    $this->assertEquals('ss_category:[* TO *]', $fq[1], 'Tag not found in unrelated second filter query');
+    $this->assertEquals('ss_category:"article_category"', $fq[0]['query'], 'Tag found in tagged first filter query');
+    $this->assertEquals(['facet:tagtosearchfor' => 'facet:tagtosearchfor'], $fq[0]['tags'], 'Tag found in tagged first filter query');
+    $this->assertEquals('ss_category:[* TO *]', $fq[1]['query'], 'Tag not found in unrelated second filter query');
+    $this->assertEquals([], $fq[1]['tags'], 'Tag found in tagged first filter query');
   }
 
   /**
