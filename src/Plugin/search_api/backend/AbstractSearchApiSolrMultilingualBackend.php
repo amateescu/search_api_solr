3b1a2d2386e8b247dd4a290cb71910e09856f195
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2016-07-07 19:24:59 +0200

    Issue #2762397 by mkalkbrenner: Undefined property: stdClass::$item_id

--- src/Plugin/search_api/backend/AbstractSearchApiSolrMultilingualBackend.php
+++ src/Plugin/search_api/backend/AbstractSearchApiSolrMultilingualBackend.php
@@ -283,7 +283,7 @@ abstract class AbstractSearchApiSolrMultilingualBackend extends SearchApiSolrBac
     $doc_languages = [];
 
     foreach ($data->response->docs as $doc) {
-      $language_id = $doc_languages[$this->createId($index->id(), $doc->item_id)] = $doc->{$field_names[SEARCH_API_LANGUAGE_FIELD_NAME]};
+      $language_id = $doc_languages[$this->createId($index->id(), $doc->{SEARCH_API_ID_FIELD_NAME})] = $doc->{$field_names[SEARCH_API_LANGUAGE_FIELD_NAME]};
       foreach (array_keys(get_object_vars($doc)) as $language_specific_field_name) {
         $field_name = Utility::getSolrDynamicFieldNameForLanguageSpecificSolrDynamicFieldName($language_specific_field_name);
         if ($field_name != $language_specific_field_name) {
