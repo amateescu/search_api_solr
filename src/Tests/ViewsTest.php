3d48064efb2d642ab24ce2e79479b34c290276c8
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2016-07-29 17:36:17 +0200

    Issue #2775497 by mkalkbrenner: Commit of #2612104 broke this tests

--- src/Tests/ViewsTest.php
+++ src/Tests/ViewsTest.php
@@ -3,7 +3,7 @@
 namespace Drupal\search_api_solr\Tests;
 
 use Drupal\search_api\Entity\Index;
-use Drupal\search_api\Tests\WebTestBase;
+use Drupal\simpletest\WebTestBase as SimpletestWebTestBase;
 
 /**
  * Tests the Views integration of the Search API.
@@ -26,16 +26,18 @@ class ViewsTest extends \Drupal\search_api\Tests\ViewsTest {
    */
   public function setUp() {
     // Skip parent::setUp().
-    WebTestBase::setUp();
+    SimpletestWebTestBase::setUp();
 
     // Swap database backend for Solr backend.
     $config_factory = \Drupal::configFactory();
-    $config_factory->getEditable('search_api.index.database_search_index')->delete();
+    $config_factory->getEditable('search_api.index.database_search_index')
+      ->delete();
     $config_factory->rename('search_api.index.solr_search_index', 'search_api.index.database_search_index');
-    $config_factory->getEditable('search_api.index.database_search_index')->set('id', 'database_search_index')->save();
+    $config_factory->getEditable('search_api.index.database_search_index')
+      ->set('id', 'database_search_index')
+      ->save();
 
     // Now do the same as parent::setUp().
-    $this->setUpExampleStructure();
     \Drupal::getContainer()
       ->get('search_api.index_task_manager')
       ->addItemsAll(Index::load($this->indexId));
