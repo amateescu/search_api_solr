3bae5fe12a96e2a3cb260ad6353ed8746b88d4a0
David Cameron <david@cadeyrn.us>
2017-03-12 19:24:16 -0500

    Added SolrDocument::getPropertyDefinitions().

--- src/Plugin/search_api/datasource/SolrDocument.php
+++ src/Plugin/search_api/datasource/SolrDocument.php
@@ -8,6 +8,7 @@ use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\Core\TypedData\ComplexDataInterface;
 use Drupal\search_api\Datasource\DatasourcePluginBase;
 use Drupal\search_api\Plugin\PluginFormTrait;
+use Drupal\search_api_solr_datasource\SolrFieldManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -24,13 +25,44 @@ class SolrDocument extends DatasourcePluginBase implements PluginFormInterface {
   use PluginFormTrait;
 
   /**
+   * The Solr field manager.
+   *
+   * @var \Drupal\search_api_solr_datasource\SolrFieldManagerInterface
+   */
+  protected $solrFieldManager;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     /** @var static $datasource */
     $datasource = parent::create($container, $configuration, $plugin_id, $plugin_definition);
 
+    $datasource->setSolrFieldManager($container->get('solr_field.manager'));
+
     return $datasource;
+  }
+
+  /**
+   * Sets the Solr field manager.
+   *
+   * @param \Drupal\search_api_solr_datasource\SolrFieldManagerInterface $solr_field_manager
+   *   The new entity field manager.
+   *
+   * @return $this
+   */
+  public function setSolrFieldManager(SolrFieldManagerInterface $solr_field_manager) {
+    $this->solrFieldManager = $solr_field_manager;
+    return $this;
+  }
+
+  /**
+   * Returns the Solr field manager.
+   *
+   * @return \Drupal\search_api_solr_datasource\SolrFieldManagerInterface
+   */
+  public function getSolrFieldManager() {
+    return $this->solrFieldManager ?: \Drupal::getContainer()->get('solr_field.manager');
   }
 
   /**
@@ -38,6 +70,15 @@ class SolrDocument extends DatasourcePluginBase implements PluginFormInterface {
    */
   public function getItemId(ComplexDataInterface $item) {
     ;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    // @todo Handle IndexInterface::getServerInstance() returning NULL.
+    $fields = $this->getSolrFieldManager()->getFieldDefinitions($this->index->getServerInstance());
+    return $fields;
   }
 
   /**
