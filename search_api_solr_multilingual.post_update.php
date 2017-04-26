ba96125aef3fce1c78c33bac47308c6f97d25e84
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2017-04-27 00:37:23 +0200

    Issue #2873163 by mkalkbrenner: Greek is broken

--- search_api_solr_multilingual.post_update.php
+++ search_api_solr_multilingual.post_update.php
@@ -30,3 +30,21 @@ function search_api_solr_multilingual_post_update_replace_solr_field_types_2() {
     search_api_solr_multilingual_delete_and_reinstall_all_field_types();
   }
 }
+
+/**
+ * Re-installs Greek Solr Field Type.
+ */
+function search_api_solr_multilingual_post_update_replace_solr_field_type_el2() {
+  $storage = \Drupal::entityTypeManager()->getStorage('solr_field_type');
+  if ($field_type = $storage->load('text_el_4_5_0')) {
+    $storage->delete([$field_type]);
+  }
+
+  /** @var \Drupal\Core\Config\ConfigInstallerInterface $config_installer */
+  $config_installer = \Drupal::service('config.installer');
+  $config_installer->installDefaultConfig('module', 'search_api_solr_multilingual');
+  $restrict_by_dependency = [
+    'module' => 'search_api_solr_multilingual',
+  ];
+  $config_installer->installOptionalConfig(NULL, $restrict_by_dependency);
+}
