4c109825c59b3c7a8e9e5e47df6b2ced35ef4a07
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2017-05-11 20:23:22 +0200

    Issue #2877762 by mkalkbrenner: Execute location tests on multilingual backend

--- tests/src/Kernel/SearchApiSolrMultilingualLocationTest.php
+++ tests/src/Kernel/SearchApiSolrMultilingualLocationTest.php
@@ -8,7 +8,7 @@ use Drupal\Tests\search_api_solr\Kernel\SearchApiSolrLocationTest;
 /**
  * Tests index and search capabilities using the Solr search backend.
  *
- * @group search_api_solr
+ * @group search_api_solr_multilingual
  */
 class SearchApiSolrMultilingualLocationTest extends SearchApiSolrLocationTest {
 
