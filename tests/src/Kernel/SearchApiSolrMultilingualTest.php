3ca24550ee12ff06d4ed8a81fb4684f1ccd4f0fd
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2017-09-21 09:00:09 +0200

    Issue #2894625 by pandaeskimo, mkalkbrenner: Reconcile "Limit to current content language" configuration

--- tests/src/Kernel/SearchApiSolrMultilingualTest.php
+++ tests/src/Kernel/SearchApiSolrMultilingualTest.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\Tests\search_api_solr_multilingual\Kernel;
 
+use Drupal\Core\Language\LanguageInterface;
 use Drupal\search_api\Entity\Server;
+use Drupal\search_api\Utility\Utility;
 use Drupal\Tests\search_api\Kernel\BackendTestBase;
 use Drupal\Tests\search_api_solr\Kernel\SearchApiSolrTest;
 
@@ -179,6 +181,68 @@ class SearchApiSolrMultilingualTest extends SearchApiSolrTest {
   }
 
   /**
+   * Tests language limiting via options.
+   */
+  public function testLanguageLimitedByOptions() {
+    // Only run further tests if we have a Solr core available.
+    if ($this->solrAvailable) {
+      $this->insertMultilingualExampleContent();
+      $this->indexItems($this->indexId);
+
+      $server = $this->getIndex()->getServerInstance();
+      $config = $server->getBackendConfig();
+
+      $config['sasm_limit_search_page_to_content_language'] = FALSE;
+      $server->setBackendConfig($config)->save();
+      $this->assertFalse($this->getIndex()->getServerInstance()->getBackendConfig()['sasm_limit_search_page_to_content_language']);
+
+      $config['sasm_search_page_include_language_independent'] = FALSE;
+      $server->setBackendConfig($config)->save();
+      $this->assertFalse($this->getIndex()->getServerInstance()->getBackendConfig()['sasm_search_page_include_language_independent']);
+
+      // Stemming "en":
+      // gene => gene
+      // genes => gene
+      //
+      // Stemming "de":
+      // Gen => gen
+      // Gene => gen
+      $results = $this->buildSearch('gene', [], ['body'])->execute();
+      $this->assertResults([1 => 'en', 2 => 'en', 3 => 'de', 4 => 'de', 5 => 'de-at', 6 => 'de-at'], $results, 'Search all languages for "gene".');
+
+      $config['sasm_limit_search_page_to_content_language'] = TRUE;
+      $server->setBackendConfig($config)->save();
+      $this->assertTrue($this->getIndex()->getServerInstance()->getBackendConfig()['sasm_limit_search_page_to_content_language']);
+
+      // Current content language is "en".
+      $results = $this->buildSearch('gene', [], ['body'])->execute();
+      $this->assertResults([1 => 'en', 2 => 'en'], $results, 'Search content language for "gene".');
+
+      // A query created by Views must not be overruled.
+      $results = $this->buildSearch('gene', [], ['body'])->addTag('views')->execute();
+      $this->assertResults([1 => 'en', 2 => 'en', 3 => 'de', 4 => 'de', 5 => 'de-at', 6 => 'de-at'], $results, 'Search all languages for "gene".');
+
+      $config['sasm_search_page_include_language_independent'] = TRUE;
+      $server->setBackendConfig($config)->save();
+      $this->assertTrue($this->getIndex()->getServerInstance()->getBackendConfig()['sasm_search_page_include_language_independent']);
+
+      $results = $this->buildSearch('gene', [], ['body'])->execute();
+      $this->assertResults([1 => 'en', 2 => 'en', 7 => LanguageInterface::LANGCODE_NOT_SPECIFIED, 8 => LanguageInterface::LANGCODE_NOT_APPLICABLE], $results, 'Search content and unspecified language for "gene".');
+
+      $config['sasm_limit_search_page_to_content_language'] = FALSE;
+      $server->setBackendConfig($config)->save();
+      $this->assertFalse($this->getIndex()->getServerInstance()->getBackendConfig()['sasm_limit_search_page_to_content_language']);
+
+      $results = $this->buildSearch('gene', [], ['body'])->execute();
+      $this->assertResults([1 => 'en', 2 => 'en', 3 => 'de', 4 => 'de', 5 => 'de-at', 6 => 'de-at', 7 => LanguageInterface::LANGCODE_NOT_SPECIFIED, 8 => LanguageInterface::LANGCODE_NOT_APPLICABLE], $results, 'Search all and unspecified languages for "gene".');
+    }
+    else {
+      $this->assertTrue(TRUE, 'Error: The Solr instance could not be found. Please enable a multi-core one on http://localhost:8983/solr/d8');
+    }
+
+  }
+
+  /**
    * Creates several test entities.
    */
   protected function insertMultilingualExampleContent() {
@@ -218,8 +282,50 @@ class SearchApiSolrMultilingualTest extends SearchApiSolrTest {
       'type' => 'item',
       'langcode' => 'de-at',
     ));
+    $this->addTestEntity(7, array(
+      'name' => 'und 7',
+      'body' => 'gene',
+      'type' => 'item',
+      'langcode' => LanguageInterface::LANGCODE_NOT_SPECIFIED,
+    ));
+    $this->addTestEntity(8, array(
+      'name' => 'zxx 8',
+      'body' => 'gene',
+      'type' => 'item',
+      'langcode' => LanguageInterface::LANGCODE_NOT_APPLICABLE,
+    ));
     $count = \Drupal::entityQuery('entity_test_mulrev_changed')->count()->execute();
-    $this->assertEquals(6, $count, "$count items inserted.");
+    $this->assertEquals(8, $count, "$count items inserted.");
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * If the list of entity ids contains language codes it will be handled here,
+   * otherwise it will be handed over to the parent implementation.
+   *
+   * @param array $entity_ids
+   *   An array of entity IDs or an array keyed by entity IDs and langcodes as
+   *   values.
+   *
+   * @return string[]
+   *   An array of item IDs.
+   */
+  protected function getItemIds(array $entity_ids) {
+    $item_ids = [];
+    if (!empty($entity_ids)) {
+      $keys = array_keys($entity_ids);
+      $first_key = reset($keys);
+      if (0 === $first_key) {
+        return parent::getItemIds($entity_ids);
+      }
+      else {
+        foreach ($entity_ids as $id => $langcode) {
+          $item_ids[] = Utility::createCombinedId('entity:entity_test_mulrev_changed', $id . ':' . $langcode);
+        }
+      }
+    }
+    return $item_ids;
   }
 
 }
