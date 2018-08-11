bdfcdd521f25eca5b7fa7076b6d584a55f92e7ea
danph <danph@81431.no-reply.drupal.org>
2018-08-11 14:28:18 +0200

    Issue #2989375 by dpi, mkalkbrenner, jibran: Remove unable to connect message and field caching

--- src/SolrFieldManager.php
+++ src/SolrFieldManager.php
@@ -6,10 +6,12 @@ use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Cache\UseCacheBackendTrait;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\search_api\Entity\Server;
 use Drupal\search_api\LoggerTrait;
+use Drupal\search_api\ServerInterface;
 use Drupal\search_api_solr\TypedData\SolrFieldDefinition;
+use Psr\Log\LoggerInterface;
 
 /**
  * Manages the discovery of Solr fields.
@@ -28,13 +30,26 @@ class SolrFieldManager implements SolrFieldManagerInterface {
   protected $fieldDefinitions;
 
   /**
+   * Storage for Search API servers.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $serverStorage;
+
+  /**
    * Constructs a new SorFieldManager.
    *
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache_backend
    *   The cache backend.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   Logger for Search API.
    */
-  public function __construct(CacheBackendInterface $cache_backend) {
+  public function __construct(CacheBackendInterface $cache_backend, EntityTypeManagerInterface $entityTypeManager, LoggerInterface $logger) {
     $this->cacheBackend = $cache_backend;
+    $this->serverStorage = $entityTypeManager->getStorage('search_api_server');
+    $this->setLogger($logger);
   }
 
   /**
@@ -47,10 +62,20 @@ class SolrFieldManager implements SolrFieldManagerInterface {
       if ($cache = $this->cacheGet($cid)) {
         $field_definitions = $cache->data;
       }
-      elseif ($field_definitions = $this->buildFieldDefinitions($server_id)) {
-        // Only cache the field definitions if they aren't empty.
-        $this->cacheSet($cid, $field_definitions, Cache::PERMANENT, ['search_api_server' => $server_id]);
+      else {
+        /** @var \Drupal\search_api\ServerInterface|null $server */
+        $server = $this->serverStorage->load($server_id);
+        // Load the server entity.
+        if ($server === NULL) {
+          throw new \InvalidArgumentException('The Search API server could not be loaded.');
+        }
+
+        // Don't attempt to connect to server if config is disabled. Cache will
+        // clear itself when server config is enabled again.
+        $field_definitions = $server->status() ? $this->buildFieldDefinitions($server) : [];
+        $this->cacheSet($cid, $field_definitions, Cache::PERMANENT, $server->getCacheTagsToInvalidate());
       }
+
       $this->fieldDefinitions[$server_id] = $field_definitions;
     }
     return $this->fieldDefinitions[$server_id];
@@ -59,7 +84,7 @@ class SolrFieldManager implements SolrFieldManagerInterface {
   /**
    * Builds the field definitions for a Solr server from its Luke handler.
    *
-   * @param string $server_id
+   * @param \Drupal\search_api\ServerInterface $server
    *   The server from which we are retrieving field information.
    *
    * @return \Drupal\search_api_solr\TypedData\SolrFieldDefinitionInterface[]
@@ -67,12 +92,7 @@ class SolrFieldManager implements SolrFieldManagerInterface {
    *
    * @throws \InvalidArgumentException
    */
-  protected function buildFieldDefinitions($server_id) {
-    // Load the server entity.
-    $server = Server::load($server_id);
-    if ($server === NULL) {
-      throw new \InvalidArgumentException('The Search API server could not be loaded.');
-    }
+  protected function buildFieldDefinitions(ServerInterface $server) {
     $backend = $server->getBackend();
     if (!$backend instanceof SolrBackendInterface) {
       throw new \InvalidArgumentException("The Search API server's backend must be an instance of SolrBackendInterface.");
@@ -114,7 +134,6 @@ class SolrFieldManager implements SolrFieldManagerInterface {
       }
     }
     catch (SearchApiSolrException $e) {
-      \Drupal::messenger()->addError($this->t('Could not connect to server %server, %message', ['%server' => $server->id(), '%message' => $e->getMessage()]));
       $this->getLogger()->error('Could not connect to server %server, %message', ['%server' => $server->id(), '%message' => $e->getMessage()]);
     }
     return $fields;
