d3e2fe3c9eddbe0090f10f6b2248b2d660fa9aed
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2019-01-25 23:15:50 +0100

    fixed typo

--- src/TypedData/SolrDocumentDefinition.php
+++ src/TypedData/SolrDocumentDefinition.php
@@ -36,7 +36,7 @@ class SolrDocumentDefinition extends ComplexDataDefinitionBase implements SolrDo
    * {@inheritdoc}
    */
   public static function createFromDataType($data_type) {
-    // The data type should be in the form of "solr_document:$server_id".
+    // The data type should be in the form of "solr_document:$index_id".
     $parts = explode(':', $data_type, 2);
     if ($parts[0] != 'solr_document') {
       throw new \InvalidArgumentException('Data type must be in the form of "solr_document:INDEX_ID".');
