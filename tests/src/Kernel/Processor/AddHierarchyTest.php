c333a1b46b910cf654375ee11789b2676bd2d860
Herve Donner <hervedonner@gmail.com>
2019-02-25 10:59:39 +0100

    Issue #3035491: Coding standards in tests/src/Kernel/Processor/AddHierarchyTest.php

--- tests/src/Kernel/Processor/AddHierarchyTest.php
+++ tests/src/Kernel/Processor/AddHierarchyTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\search_api_solr\Kernel\Processor;
 
+use Drupal\Tests\search_api\Kernel\Processor\AddHierarchyTest as SearchApiAddHierarchyTest;
+
 /**
  * Tests the "Hierarchy" processor.
  *
@@ -11,7 +13,7 @@ namespace Drupal\Tests\search_api_solr\Kernel\Processor;
  *
  * @coversDefaultClass \Drupal\search_api\Plugin\search_api\processor\AddHierarchy
  */
-class AddHierarchyTest extends \Drupal\Tests\search_api\Kernel\Processor\AddHierarchyTest {
+class AddHierarchyTest extends SearchApiAddHierarchyTest {
 
   use SolrBackendTrait;
 
