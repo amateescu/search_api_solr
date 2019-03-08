0157bcf9cff816f9490254c2e41f420720cde58a
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2019-03-08 16:00:56 +0100

    Issue #3038618 by mkalkbrenner: StreamingExpressionQueryHelper::setStreamingExpression() should append an optional comment to the exprssion

--- src/Utility/StreamingExpressionQueryHelper.php
+++ src/Utility/StreamingExpressionQueryHelper.php
@@ -34,8 +34,11 @@ class StreamingExpressionQueryHelper extends QueryHelper {
    * @param string $streaming_expression
    *   The streaming expression to set for this query.
    */
-  public function setStreamingExpression(QueryInterface $query, string $streaming_expression) {
-    $query->setOption('solr_streaming_expression', $streaming_expression);
+  public function setStreamingExpression(QueryInterface $query, string $streaming_expression, string $comment = '') {
+    if ($comment) {
+      $comment = ' /* ' . $comment . ' */';
+    }
+    $query->setOption('solr_streaming_expression', $streaming_expression . $comment);
   }
 
 }
