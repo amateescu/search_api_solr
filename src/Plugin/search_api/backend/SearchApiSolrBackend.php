6519939de038428b83cb74f4b76828557c0fffe7
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2019-05-31 16:40:34 +0200

    Issue #3058483 by mkalkbrenner: MLT queries contain a lot of overhead

--- src/Plugin/search_api/backend/SearchApiSolrBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrBackend.php
@@ -1250,46 +1250,55 @@ class SearchApiSolrBackend extends BackendPluginBase implements SolrBackendInter
         );
       }
 
-      // Set the list of fields to retrieve.
-      $this->setFields($solarium_query, $query->getOption('search_api_retrieved_field_values', []), $query);
-
-      // Set sorts.
-      $this->setSorts($solarium_query, $query);
-
-      // Set facet fields. setSpatial() might add more facets.
-      $this->setFacets($query, $solarium_query);
-
-      // Handle spatial filters.
-      if (isset($options['search_api_location'])) {
-        $this->setSpatial($solarium_query, $options['search_api_location'], $query);
+      if ($query->hasTag('mlt')) {
+        // For MLT we onöy fetch one document, but in multiple translations. 100
+        // translations should be enough as setting higher number of rows cause
+        // more reserved memory on distributed searches (shards or cloud).
+        $solarium_query->setStart(0);
+        $solarium_query->setRows(100);
       }
+      else {
+        // Set the list of fields to retrieve.
+        $this->setFields($solarium_query, $query->getOption('search_api_retrieved_field_values', []), $query);
 
-      // Handle spatial filters.
-      if (isset($options['search_api_rpt'])) {
-        $this->setRpt($solarium_query, $options['search_api_rpt'], $query);
+        // Set sorts.
+        $this->setSorts($solarium_query, $query);
+
+        // Set facet fields. setSpatial() might add more facets.
+        $this->setFacets($query, $solarium_query);
+
+        // Handle spatial filters.
+        if (isset($options['search_api_location'])) {
+          $this->setSpatial($solarium_query, $options['search_api_location'], $query);
+        }
+
+        // Handle spatial filters.
+        if (isset($options['search_api_rpt'])) {
+          $this->setRpt($solarium_query, $options['search_api_rpt'], $query);
+        }
+
+        // Handle field collapsing / grouping.
+        if (isset($options['search_api_grouping'])) {
+          $this->setGrouping($solarium_query, $query, $options['search_api_grouping'], $index_fields, $field_names);
+        }
+
+        // Handle spellcheck.
+        if (isset($options['search_api_spellcheck'])) {
+          $this->setSpellcheck($solarium_query, $query, $options['search_api_spellcheck']);
+        }
+
+        if (isset($options['offset'])) {
+          $solarium_query->setStart($options['offset']);
+        }
+
+        // In previous versions we set a high value for rows if no limit was set
+        // in the options. The intention was to retrieve "all" results instead of
+        // falling back to Solr's default of 10. But for Solr Cloud it turned out
+        // that independent from the real number of documents, Solr seems to
+        // allocate rows*shards memory for sorting the distributed result. That
+        // could lead to out of memory exceptions.
+        $solarium_query->setRows($query->getOption('limit', 10) ?? 10);
       }
-
-      // Handle field collapsing / grouping.
-      if (isset($options['search_api_grouping'])) {
-        $this->setGrouping($solarium_query, $query, $options['search_api_grouping'], $index_fields, $field_names);
-      }
-
-      // Handle spellcheck.
-      if (isset($options['search_api_spellcheck'])) {
-        $this->setSpellcheck($solarium_query, $query, $options['search_api_spellcheck']);
-      }
-
-      if (isset($options['offset'])) {
-        $solarium_query->setStart($options['offset']);
-      }
-
-      // In previous versions we set a high value for rows if no limit was set
-      // in the options. The intention was to retrieve "all" results instead of
-      // falling back to Solr's default of 10. But for Solr Cloud it turned out
-      // that independent from the real number of documents, Solr seems to
-      // allocate rows*shards memory for sorting the distributed result. That
-      // could lead to out of memory exceptions.
-      $solarium_query->setRows($query->getOption('limit', 10) ?? 10);
 
       foreach ($options as $option => $value) {
         if (strpos($option, 'solr_param_') === 0) {
