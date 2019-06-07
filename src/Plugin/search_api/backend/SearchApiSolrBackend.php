61fcfc902ad41c64ed3837dff4a56cfe6e20c179
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2019-06-07 15:56:34 +0200

    Issue #3060227 by mkalkbrenner, lukus: Pagination / Number of items broken for MLT block

--- src/Plugin/search_api/backend/SearchApiSolrBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrBackend.php
@@ -1251,14 +1251,9 @@ class SearchApiSolrBackend extends BackendPluginBase implements SolrBackendInter
       }
 
       if ($query->hasTag('mlt')) {
-        // Set the list of fields to retrieve.
+        // Set the list of fields to retrieve, but avoid highlighting and
+        // different overhead.
         $this->setFields($solarium_query, $query->getOption('search_api_retrieved_field_values', []), $query, FALSE);
-
-        // For MLT we onöy fetch one document, but in multiple translations. 100
-        // translations should be enough as setting higher number of rows cause
-        // more reserved memory on distributed searches (shards or cloud).
-        $solarium_query->setStart(0);
-        $solarium_query->setRows(100);
       }
       else {
         // Set the list of fields to retrieve.
@@ -1289,19 +1284,19 @@ class SearchApiSolrBackend extends BackendPluginBase implements SolrBackendInter
         if (isset($options['search_api_spellcheck'])) {
           $this->setSpellcheck($solarium_query, $query, $options['search_api_spellcheck']);
         }
-
-        if (isset($options['offset'])) {
-          $solarium_query->setStart($options['offset']);
-        }
-
-        // In previous versions we set a high value for rows if no limit was set
-        // in the options. The intention was to retrieve "all" results instead of
-        // falling back to Solr's default of 10. But for Solr Cloud it turned out
-        // that independent from the real number of documents, Solr seems to
-        // allocate rows*shards memory for sorting the distributed result. That
-        // could lead to out of memory exceptions.
-        $solarium_query->setRows($query->getOption('limit', 10) ?? 10);
       }
+
+      if (isset($options['offset'])) {
+        $solarium_query->setStart($options['offset']);
+      }
+
+      // In previous versions we set a high value for rows if no limit was set
+      // in the options. The intention was to retrieve "all" results instead of
+      // falling back to Solr's default of 10. But for Solr Cloud it turned out
+      // that independent from the real number of documents, Solr seems to
+      // allocate rows*shards memory for sorting the distributed result. That
+      // could lead to out of memory exceptions.
+      $solarium_query->setRows($query->getOption('limit', 10) ?? 10);
 
       foreach ($options as $option => $value) {
         if (strpos($option, 'solr_param_') === 0) {
