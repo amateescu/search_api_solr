f423e5888bd62ecce941649b951cf684530c5a6e
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2019-07-02 14:39:29 +0200

    Issue #3065385 by mkalkbrenner: Drush commands entirely broken because of Search API changes

--- src/Commands/SearchApiSolrCommands.php
+++ src/Commands/SearchApiSolrCommands.php
@@ -12,6 +12,7 @@ use Drupal\search_api_solr\SolrCloudConnectorInterface;
 use Drupal\search_api_solr\Utility\CommandHelper;
 use Drush\Commands\DrushCommands;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Defines Drush commands for the Search API Solr.
@@ -34,12 +35,14 @@ class SearchApiSolrCommands extends DrushCommands implements StdinAwareInterface
    *   The entity type manager.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
    *   The module handler.
+   * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, ModuleHandlerInterface $moduleHandler) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, ModuleHandlerInterface $moduleHandler, EventDispatcherInterface $eventDispatcher) {
     parent::__construct();
-    $this->commandHelper = new CommandHelper($entityTypeManager, $moduleHandler, 'dt');
+    $this->commandHelper = new CommandHelper($entityTypeManager, $moduleHandler, $eventDispatcher, 'dt');
   }
 
   /**
