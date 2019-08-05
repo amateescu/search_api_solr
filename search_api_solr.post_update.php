30df1407eae4568a55548018a6d04de8eeaff9c7
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2019-08-05 14:31:11 +0200

    Issue #3072681 by mkalkbrenner: Clean-up 1.x to 3.x upgrade path and avoid obsolete update hooks

--- search_api_solr.post_update.php
+++ search_api_solr.post_update.php
@@ -2,33 +2,35 @@
 
 /**
  * Delete Solr 4 and 5 field types.
- *
- * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
- * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
- * @throws \Drupal\Core\Entity\EntityStorageException
  */
 function search_api_solr_post_update_8204_replace_solr_4_field_types() {
-  $storage = \Drupal::entityTypeManager()->getStorage('solr_field_type');
-  $storage->delete($storage->loadMultiple([
-    'm_text_und_5_2_0',
-    'text_und_4_5_0',
-    'm_text_de_5_2_0',
-    'm_text_en_5_2_0',
-    'm_text_nl_5_2_0',
-    'text_cs_5_0_0',
-    'text_de_4_5_0',
-    'text_de_5_0_0',
-    'text_de_scientific_5_0_0',
-    'text_el_4_5_0',
-    'text_en_4_5_0',
-    'text_es_4_5_0',
-    'text_fi_4_5_0',
-    'text_fr_4_5_0',
-    'text_it_4_5_0',
-    'text_nl_4_5_0',
-    'text_ru_4_5_0',
-    'text_uk_4_5_0',
-  ]));
+  try {
+    $storage = \Drupal::entityTypeManager()->getStorage('solr_field_type');
+    $storage->delete($storage->loadMultiple([
+      'm_text_und_5_2_0',
+      'text_und_4_5_0',
+      'm_text_de_5_2_0',
+      'm_text_en_5_2_0',
+      'm_text_nl_5_2_0',
+      'text_cs_5_0_0',
+      'text_de_4_5_0',
+      'text_de_5_0_0',
+      'text_de_scientific_5_0_0',
+      'text_el_4_5_0',
+      'text_en_4_5_0',
+      'text_es_4_5_0',
+      'text_fi_4_5_0',
+      'text_fr_4_5_0',
+      'text_it_4_5_0',
+      'text_nl_4_5_0',
+      'text_ru_4_5_0',
+      'text_uk_4_5_0',
+    ]));
+  }
+  catch (\Exception $e) {
+    // Don't break the upgrade, ignore the error because it is just nice to have
+    // cleanup.
+  }
 }
 
 /**
