446bd5709a6dcb132c30cbb0eb6768bc52570136
Markus Kalkbrenner <markus.kalkbrenner@biologis.com>
2020-06-09 14:20:15 +0200

    Issue #3150211 by mkalkbrenner: Run tests aginast solarium master

--- tests/src/Kernel/SearchApiSolrLocationTest.php
+++ tests/src/Kernel/SearchApiSolrLocationTest.php
@@ -12,7 +12,7 @@ use Drupal\search_api\Entity\Server;
  *
  * @group search_api_solr
  * @group not_solr4
- * @group not_drupal9
+ * @group not_drupal9.0
  */
 class SearchApiSolrLocationTest extends SolrBackendTestBase {
 
